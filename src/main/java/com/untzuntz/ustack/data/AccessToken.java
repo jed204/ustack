9fa149e8162ba47a387b12644965e842dc69a79a
Jed Danner <jed@textrecruit.com>
2018-12-17 16:24:36 -0800

    JWT Auth

--- src/main/java/com/untzuntz/ustack/data/AccessToken.java
+++ src/main/java/com/untzuntz/ustack/data/AccessToken.java
@@ -1,12 +1,29 @@
 package com.untzuntz.ustack.data;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.untzuntz.ustack.exceptions.AuthExceptionAuthError;
+import org.apache.commons.lang.StringUtils;
 import org.jasypt.util.text.BasicTextEncryptor;
 
 import com.Ostermiller.util.Base64;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.UUID;
+
 public class AccessToken {
 
-	private static String INTERNAL_KEY;
+	public static String INTERNAL_KEY;
+	public static String INTERNAL_JWT_KEY;
+	public static String JWT_ISSUER;
 
 	private BasicTextEncryptor textEncryptor;
 	private BasicTextEncryptor getEncryptor()
@@ -20,18 +37,79 @@ public class AccessToken {
 		return textEncryptor;
 	}
 
+	private static JWTVerifier getJwtVerifier(String issuer) {
+		return JWT.require(getAlgorithm())
+				.withIssuer(issuer)
+				.build(); // Reusable verifier instance
+	}
+
+	private static Algorithm getAlgorithm() {
+		try {
+			return Algorithm.HMAC256("s]DTV7EqgZ6BAiVgW" + INTERNAL_JWT_KEY + "qqKN6n)MxM{x(FRot?93zNe^8gR");
+		} catch (UnsupportedEncodingException e) {
+			// major fail
+		}
+		return null;
+	}
+
+	public static DecodedJWT decodeJwt(String encodedJWT) throws AuthExceptionAuthError {
+		DecodedJWT jwt;
+		try {
+			jwt = getJwtVerifier(JWT_ISSUER).verify(encodedJWT);
+		} catch (JWTVerificationException exception) {
+			System.err.println("exception = " + exception);
+			throw new AuthExceptionAuthError();
+		}
+		return jwt;
+	}
+
+	public static String encodeJwt(String clientId, String userName, long expirationAge, String ipAddress, Map<String,String> custom) throws AuthExceptionAuthError {
+
+		String token;
+		try {
+			JWTCreator.Builder tokenBuilder = JWT.create()
+					.withExpiresAt(new Date((System.currentTimeMillis() + expirationAge)))
+					.withIssuer(JWT_ISSUER)
+					.withClaim("id", UUID.randomUUID().toString())
+					.withClaim("clientId", clientId)
+					.withClaim("userName", userName);
+
+			if (StringUtils.isNotEmpty(ipAddress)) {
+				tokenBuilder.withClaim("ipAddress", ipAddress);
+			}
+
+			if (custom != null) {
+
+				Iterator<String> it = custom.keySet().iterator();
+				while (it.hasNext()) {
+					String key = it.next();
+					tokenBuilder.withClaim(key, custom.get(custom.get(key)));
+				}
+
+			}
+
+			token = tokenBuilder.sign(getAlgorithm());
+
+		} catch (JWTCreationException exception) {
+			throw new AuthExceptionAuthError();
+		}
+
+		return token;
+
+	}
+
 	public static String encode(String clientId, String userName, long expirationAge)
 	{
 		AccessToken at = new AccessToken();
-		
+
 		StringBuffer buf = new StringBuffer();
 		buf.append(clientId).append("|");
 		buf.append(userName).append("|");
 		buf.append((System.currentTimeMillis() + expirationAge));
-		
+
 		return Base64.encode(at.getEncryptor().encrypt(buf.toString()));
 	}
-	
+
 	public static AccessTokenDetails decode(String value)
 	{
 		if (value == null)
@@ -48,7 +126,7 @@ public class AccessToken {
 			return null;
 		
 		String[] spl = decrypted.split("\\|");
-		if (spl.length != 3)
+		if (spl.length < 3)
 			return null;
 		
 		return at.getAccessDetails(spl);
@@ -60,7 +138,7 @@ public class AccessToken {
 		ret.clientId = spl[0];
 		ret.userName = spl[1];
 		ret.expirationAge = Long.valueOf(spl[2]);
-		
+
 		return ret;
 
 	}
@@ -70,6 +148,24 @@ public class AccessToken {
 		private String clientId;
 		private String userName;
 		private long expirationAge;
+		private String ipAddress;
+		private String customData;
+
+		public String getIpAddress() {
+			return ipAddress;
+		}
+
+		public void setIpAddress(String ipAddress) {
+			this.ipAddress = ipAddress;
+		}
+
+		public String getCustomData() {
+			return customData;
+		}
+
+		public void setCustomData(String customData) {
+			this.customData = customData;
+		}
 
 		public String getClientId() {
 			return clientId;
